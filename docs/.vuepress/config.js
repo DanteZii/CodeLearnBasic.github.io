700ac3586a9786df1c2d7420aa389bce17868d21
Dante <guge862055200@gmail.com>
2020-04-23 14:14:53 +0200

    'python页面'

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: 'LearnCodeBasic',
+    description: 'Just do it',
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Python', link: '/Python/' }
+      ]
+    }
+  }
